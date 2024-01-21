2d3bbe67b57fd7b57d8ebdb7b3d9a804287ab5c8
CarlosErz <rosariosalda25@gmail.com>
2024-01-21 12:59:03 -0600

    Add handa_fingers module and update app configuration

--- handa_fingers.py
+++ handa_fingers.py
@@ -1,134 +1,131 @@
 import cv2
 import mediapipe as mp
-#---------------------------------------------
 import serial
 import time
-#---------------------------------------------
 import math
-    
+import customtkinter as tk
+import serial.tools.list_ports
+
 # ---------------------------------------------
 # Configuración de OpenCV y MediaPipe
 # ---------------------------------------------
-mp_dibujo = mp.solutions.drawing_utils
-mp_manos = mp.solutions.hands
-cap = cv2.VideoCapture(0)
+def hands_funcion(puerto_com, camara, cantidad_servo, get_ports, active_arduino):  
+    mp_dibujo = mp.solutions.drawing_utils
+    mp_manos = mp.solutions.hands
+    cap = cv2.VideoCapture(int(camara))
 
-# ---------------------------------------------
-# Configuración de la comunicación con Arduino
-# ---------------------------------------------
-# arduino = serial.Serial('COM4', 9600)
+    try:
+        if active_arduino == "on":
+            arduino = serial.Serial(puerto_com, 9600)
+            print("Conexión con Arduino establecida.")
+        else:
+            arduino = None  # Simular que no hay comunicación con Arduino
+            print("La comunicación con Arduino está desactivada.")
 
-# ---------------------------------------------
-# Bucle principal
-# ---------------------------------------------
+        # Resto del código de la función...
 
+    except serial.serialutil.SerialException as e:
+        print(f"Error al abrir el puerto COM: {e}")
+        arduino = None  # Simular que no hay comunicación con Arduino
 
+    def calcular_distancia(punto_dedo, landmark_wrist):
+        return math.sqrt(
+            (punto_dedo.x - landmark_wrist.x) ** 2 +
+            (punto_dedo.y - landmark_wrist.y) ** 2 +
+            (punto_dedo.z - landmark_wrist.z) ** 2
+        )
 
-#funciones 
-#---------------------------------------------
+    def redondear_distancia(distancia):
+        return round(distancia, 1) if distancia > 0.19 else 0.19
 
-def calcular_distancia(punto_dedo, landmark_wrist):
-                    return math.sqrt(
-                        (punto_dedo.x - landmark_wrist.x) ** 2 +
-                        (punto_dedo.y - landmark_wrist.y) ** 2 +
-                        (punto_dedo.z - landmark_wrist.z) ** 2
-                    )
-                    
-def redondear_distancia(distancia):
-                    return round(distancia, 1) if distancia > 0.19 else 0.19
-                    
-def dibujar_rectangulo_y_texto(frame, punto_dedo, color_rectangulo=(0, 255, 0), color_texto=(255, 255, 255)):
-    x, y = int(punto_dedo.x * frame.shape[1]), int(punto_dedo.y * frame.shape[0])
-    cv2.rectangle(frame, (x - 10, y - 10), (x + 10, y + 10), color_rectangulo, 2)
-    distancia_dedo = calcular_distancia(punto_dedo, landmarks.landmark[mp_manos.HandLandmark.WRIST])
-    distancia_dedo = redondear_distancia(distancia_dedo)
-    cv2.putText(frame, f"{distancia_dedo}", (x - 20, y - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color_texto, 2)
-    
-#---------------------------------------------
-                      
-with mp_manos.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as manos:
-    prev_puntas_dedos = [None, None, None, None, None]
-    last_send_time = time.time()
+    def dibujar_rectangulo_y_texto(frame, punto_dedo, color_rectangulo=(0, 255, 0), color_texto=(255, 255, 255)):
+        x, y = int(punto_dedo.x * frame.shape[1]), int(punto_dedo.y * frame.shape[0])
+        cv2.rectangle(frame, (x - 10, y - 10), (x + 10, y + 10), color_rectangulo, 2)
+        distancia_dedo = calcular_distancia(punto_dedo, landmarks.landmark[mp_manos.HandLandmark.WRIST])
+        distancia_dedo = redondear_distancia(distancia_dedo)
+        cv2.putText(frame, f"{distancia_dedo}", (x - 20, y - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color_texto, 2)
 
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            continue
+    with mp_manos.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as manos:
+        prev_puntas_dedos = [None, None, None, None, None]
+        last_send_time = time.time()
 
-        frame = cv2.flip(frame, 1)
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        resultados = manos.process(frame_rgb)
-        
-        cv2.putText(frame, f"@Eanz", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
-        
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                continue
 
-        if resultados.multi_hand_landmarks:
-            for landmarks in resultados.multi_hand_landmarks:
-                puntas_dedos = [
-                    landmarks.landmark[mp_manos.HandLandmark.THUMB_TIP],
-                    landmarks.landmark[mp_manos.HandLandmark.INDEX_FINGER_TIP],
-                    landmarks.landmark[mp_manos.HandLandmark.MIDDLE_FINGER_TIP],
-                    landmarks.landmark[mp_manos.HandLandmark.RING_FINGER_TIP],
-                    landmarks.landmark[mp_manos.HandLandmark.PINKY_TIP]
-                ]
-                
-                umbral_cierre = 0.19
-                umbral_cierre_gordo = 18
+            frame = cv2.flip(frame, 1)
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            resultados = manos.process(frame_rgb)
 
-                acciones_dedos = {
-                    'gordo': '0',
-                    'pinky': '0',
-                    'medio': '0',
-                    'indice': '0',
-                    'anular': '0'
-                }
-             
-                    
-            for i, punto_dedo in enumerate(puntas_dedos):
-                dibujar_rectangulo_y_texto(frame, punto_dedo)
-                distancia_gordo = calcular_distancia(puntas_dedos[0], landmarks.landmark[mp_manos.HandLandmark.WRIST])
-                distancia_indice_palma = calcular_distancia(puntas_dedos[1], landmarks.landmark[mp_manos.HandLandmark.WRIST])
-                
-                distancia_middele = calcular_distancia(puntas_dedos[2], landmarks.landmark[mp_manos.HandLandmark.WRIST])
-                
-                distancia_anular = calcular_distancia(puntas_dedos[3], landmarks.landmark[mp_manos.HandLandmark.WRIST])
-                
-                distancia_pinky = calcular_distancia(puntas_dedos[4], landmarks.landmark[mp_manos.HandLandmark.WRIST])
+            cv2.putText(frame, f"@Eanz", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
 
-               
+            if resultados.multi_hand_landmarks:
+                for landmarks in resultados.multi_hand_landmarks:
+                    puntas_dedos = [
+                        landmarks.landmark[mp_manos.HandLandmark.THUMB_TIP],
+                        landmarks.landmark[mp_manos.HandLandmark.INDEX_FINGER_TIP],
+                        landmarks.landmark[mp_manos.HandLandmark.MIDDLE_FINGER_TIP],
+                        landmarks.landmark[mp_manos.HandLandmark.RING_FINGER_TIP],
+                        landmarks.landmark[mp_manos.HandLandmark.PINKY_TIP]
+                    ]
 
-                distancia_gordo = distancia_gordo * 100
-                distancia_indice_palma = redondear_distancia(distancia_indice_palma)
-                distancia_middele = redondear_distancia(distancia_middele)
-                distancia_anular = redondear_distancia(distancia_anular)
-                distancia_pinky = redondear_distancia(distancia_pinky)
+                    umbral_cierre = 0.19
+                    umbral_cierre_gordo = 18
 
-                if distancia_gordo <= umbral_cierre_gordo:
-                    acciones_dedos['gordo'] = '1'
-                if distancia_indice_palma <= umbral_cierre:
-                    acciones_dedos['indice'] = '1'
-                if distancia_middele <= umbral_cierre:
-                    acciones_dedos['medio'] = '1'
-                if distancia_anular <= umbral_cierre:
-                    acciones_dedos['anular'] = '1'
-                if distancia_pinky <= umbral_cierre:
-                    acciones_dedos['pinky'] = '1'
-                    
-                #arduino.write(f"{acciones_dedos['gordo']}{acciones_dedos['indice']}{acciones_dedos['medio']}{acciones_dedos['anular']}{acciones_dedos['pinky']}".encode())
-                
-                print(f"{acciones_dedos['gordo']}{acciones_dedos['indice']}{acciones_dedos['medio']}{acciones_dedos['anular']}{acciones_dedos['pinky']}")
-                
-                    
+                    acciones_dedos = {
+                        'gordo': '0',
+                        'pinky': '0',
+                        'medio': '0',
+                        'indice': '0',
+                        'anular': '0'
+                    }
 
-        cv2.imshow("Manos", frame)
+                for i, punto_dedo in enumerate(puntas_dedos):
+                    dibujar_rectangulo_y_texto(frame, punto_dedo)
+                    distancia_gordo = calcular_distancia(puntas_dedos[0], landmarks.landmark[mp_manos.HandLandmark.WRIST])
+                    distancia_indice_palma = calcular_distancia(puntas_dedos[1], landmarks.landmark[mp_manos.HandLandmark.WRIST])
 
-        if cv2.waitKey(10) & 0xFF == ord("q"):
-            break
+                    distancia_middele = calcular_distancia(puntas_dedos[2], landmarks.landmark[mp_manos.HandLandmark.WRIST])
 
-cap.release()
-cv2.destroyAllWindows()
+                    distancia_anular = calcular_distancia(puntas_dedos[3], landmarks.landmark[mp_manos.HandLandmark.WRIST])
+
+                    distancia_pinky = calcular_distancia(puntas_dedos[4], landmarks.landmark[mp_manos.HandLandmark.WRIST])
+
+                    distancia_gordo = distancia_gordo * 100
+                    distancia_indice_palma = redondear_distancia(distancia_indice_palma)
+                    distancia_middele = redondear_distancia(distancia_middele)
+                    distancia_anular = redondear_distancia(distancia_anular)
+                    distancia_pinky = redondear_distancia(distancia_pinky)
+
+                    if distancia_gordo <= umbral_cierre_gordo:
+                        acciones_dedos['gordo'] = '1'
+                    if distancia_indice_palma <= umbral_cierre:
+                        acciones_dedos['indice'] = '1'
+                    if distancia_middele <= umbral_cierre:
+                        acciones_dedos['medio'] = '1'
+                    if distancia_anular <= umbral_cierre:
+                        acciones_dedos['anular'] = '1'
+                    if distancia_pinky <= umbral_cierre:
+                        acciones_dedos['pinky'] = '1'
+                    if arduino:   
+                        arduino.write(f"{acciones_dedos['gordo']}{acciones_dedos['indice']}{acciones_dedos['medio']}{acciones_dedos['anular']}{acciones_dedos['pinky']}".encode())   
+
+                    print(f"{acciones_dedos['gordo']}{acciones_dedos['indice']}{acciones_dedos['medio']}{acciones_dedos['anular']}{acciones_dedos['pinky']}")
+
+            cv2.imshow("Manos", frame)
+
+            if cv2.waitKey(10) & 0xFF == ord("q"):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+    if arduino:
+        arduino.close()
+
 # ---------------------------------------------
 # Cierre de la conexión con Arduino
 # ---------------------------------------------
-# arduino.close()
 
+# ... (resto del código)
--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import customtkinter as tk
 import serial.tools.list_ports
 import cv2
 from variables import *
+from handa_fingers import *
 
-#configuracion de la libreria / library configuration
-#---------------------------------------------------------------------------
+# Configuración de la librería / Library configuration
 tk.set_appearance_mode("dark") 
 tk.set_default_color_theme("dark-blue") 
 tk.deactivate_automatic_dpi_awareness()
-#configuracion de la app / app configuration
-#---------------------------------------------------------------------------
+
+# Configuración de la aplicación / App configuration
 app = tk.CTk() 
 app.geometry(WINDOWS_SIZE)
 app.title(TITLE)
@@ -18,18 +18,15 @@ app.iconbitmap(ICON_BLANCO)
 app.resizable(False, False)
 app.config(bg="#080013")
 
-#centrar en la pantalla / center in the screen
-#---------------------------------------------------------------------------
+# Centrar en la pantalla / Center in the screen
 app.geometry(f"+{int(app.winfo_screenwidth()/2 - int(WINDOWS_SIZE.split('x')[0])/2)}+{int(app.winfo_screenheight()/2 - int(WINDOWS_SIZE.split('x')[1])/2)}")
 
-#funciones / functions
-#---------------------------------------------------------------------------
+# Funciones / Functions
 def combobox_callback(choice):
-    print("La camara escogida es:", choice)
+    print("La cámara escogida es:", choice)
+
 def combobox_callback_port(choice):
     print("El puerto escogido es:", choice)
-
-
 
 def get_camera_names():
     camera_names = []
@@ -45,13 +42,28 @@ def get_camera_names():
         return ["No se encontraron cámaras"]
 
     return camera_names
-  
+
+def star_app():
+    hands_funcion(puerto_com, camara, cantidad_servos, get_ports,  active_arduino)
+    print("Iniciando")
+
+
+def port_com_active():
+    if switch_var.get() == "on":
+        puerto_com = get_selected_com_port()
+        print(switch_var.get())
+    else:
+        puerto_com = "No se encontraron puertos COM"
+        print(switch_var.get())
+        return False
+    
+
+
 def get_ports():
     ports = [port.device for port in serial.tools.list_ports.comports()]
     if not ports:
         return ["No se encontraron puertos COM"]
     return ports
-
 
 # Etiqueta
 label = tk.CTkLabel(app, text="CONFIGURACION DE FINGERS", font=("Arial", 18,"bold"), fg_color="#080013", text_color="white", anchor="center")
@@ -59,33 +71,36 @@ label.pack(padx=10, pady=20)
 
 # Obtener nombres de cámaras
 camera_names = get_camera_names()
-label = tk.CTkLabel(app, text="Selecciona la camara:", font=("Arial", 12), fg_color="#080013", text_color="white", anchor="center")
+label = tk.CTkLabel(app, text="Selecciona la cámara:", font=("Arial", 12), fg_color="#080013", text_color="white", anchor="center")
 label.pack()
 
-# Combobox
-combobox = tk.CTkComboBox(app, values=camera_names, command=combobox_callback , width=200, height=30, font=("Arial", 12),
-                          border_color = "#19003A",fg_color="#080013",button_color="#19003A", text_color="white", dropdown_font=("Arial", 12), dropdown_fg_color="#19003A", dropdown_text_color="white")
+# Combobox camera
+combobox = tk.CTkComboBox(app, values=camera_names, command=combobox_callback, width=200, height=30, font=("Arial", 12),
+                          border_color="#19003A", fg_color="#080013", button_color="#19003A", text_color="white", dropdown_font=("Arial", 12), dropdown_fg_color="#19003A", dropdown_text_color="white")
 combobox.set(camera_names[0])
 combobox.pack(padx=10, pady=10)
 
+switch_var = tk.StringVar(value=port_com_active)
+witch = tk.CTkSwitch(app, text="CTkSwitch", command=port_com_active,
+                                 variable=switch_var, onvalue="on", offvalue="off", state=tk.DISABLED if not get_ports() else tk.NORMAL)
+switch_var.set("on")
+witch.pack(padx=10, pady=10)
+
 label = tk.CTkLabel(app, text="Selecciona el puerto:", font=("Arial", 12), fg_color="#080013", text_color="white", anchor="center")
 label.pack()
-# Combobox
 
+# Combobox
 ports = get_ports()
-combobox = tk.CTkComboBox(app, values=ports, width=200, height=30, font=("Arial", 12),command=combobox_callback_port, fg_color="#080013", border_color = "#19003A", button_color="#19003A", text_color="white", dropdown_font=("Arial", 12),dropdown_fg_color="#19003A", dropdown_text_color="white")
-combobox.set(ports[0])
-combobox.pack(padx=10, pady=10)
+combobox_port = tk.CTkComboBox(app, values=ports, width=200, height=30, font=("Arial", 12), command=combobox_callback_port, fg_color="#080013", border_color="#19003A", button_color="#19003A", text_color="white", dropdown_font=("Arial", 12), dropdown_fg_color="#19003A", dropdown_text_color="white", state=tk.DISABLED if not ports else tk.NORMAL)
+combobox_port.set(ports[0] if ports else "No se encontraron puertos COM")
+combobox_port.pack(padx=10, pady=10)
 
 button = tk.CTkButton(app, text="Iniciar", font=("Arial", 12), 
-                      width=(200),fg_color="#080013" , border_color = "#2A0063",border_spacing=(10), 
-                      hover_color="#2A0063",border_width=(2),text_color="white", command=app.destroy, anchor="center")
-button.pack( side="bottom" , padx=10, pady=10)
+                      width=(200), fg_color="#080013", border_color="#2A0063", border_spacing=(10), 
+                      hover_color="#2A0063", border_width=(2), text_color="white", command=star_app, anchor="center")
+button.pack(side="bottom", padx=10, pady=10)
 
-
-#Valor de comunicacion a la mano / Value the comunication a hands 
-
-
+# Valor de comunicación a la mano / Value the communication to hands 
 label = tk.CTkLabel(app, text="Cuantos Servos conectar:", font=("Arial", 12), fg_color="#080013", text_color="white", anchor="center")
 label.pack()
 
@@ -94,16 +109,28 @@ def combobox_callback_servo(choice):
     value_choice = choice
     return value_choice
 
-    
 # Combobox
 servo_combobox = tk.CTkComboBox(app, values=['5','3'], command=combobox_callback_servo, width=200, height=30, font=("Arial", 12),
-                          border_color = "#19003A",fg_color="#080013",button_color="#19003A", text_color="white", dropdown_font=("Arial", 12), dropdown_fg_color="#19003A", dropdown_text_color="white")
+                          border_color="#19003A", fg_color="#080013", button_color="#19003A", text_color="white", dropdown_font=("Arial", 12), dropdown_fg_color="#19003A", dropdown_text_color="white")
 servo_combobox.set('3')
 servo_combobox.pack(padx=10, pady=10)
 
+def get_selected_camera():
+    return combobox.get()
+
+def get_selected_com_port():
+    return combobox_port.get()
+
+def get_selected_servos():
+    return servo_combobox.get()
+
+def active_arduino():
+    return switch_var.get()
 
 
-#Valor de comunicacion a la mano / Value the comunication a hands
+puerto_com = get_selected_com_port()  
+camara = get_selected_camera()  
+cantidad_servos = get_selected_servos() 
+active_arduino = active_arduino()
 
-# Iniciar el bucle principal de la interfaz
 app.mainloop()
